34707937aefbee75cf3d2c87cb60a0a6d7d11ca8
Andre Flor <andreflor96@hotmail.com>
2023-11-06 08:42:08 -0300

    Change paths to files

--- src/infra/express/server.ts
+++ src/infra/express/server.ts
@@ -1,45 +1,45 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import { PrismaUserRepository } from '../../domain/repositories/PrismaUserRepository';
-import { UserController } from '../../app/controllers/UserController';
-import { UserUseCase } from '../../app/usecases/UserUseCase';
-import { AuthService } from '../../app/services/AuthService';
-import { PrismaClient } from '@prisma/client';
+import express, { Application, Request, Response } from 'express'
+import bodyParser from 'body-parser'
+import { PrismaUserRepository } from '../../domain/repositories/PrismaUserRepository'
+import { UserController } from '../../app/controllers/UserController'
+import { UserUseCase } from '../../app/usecases/UserUseCase'
+import { AuthService } from '../../app/services/AuthService'
+import { PrismaClient } from '@prisma/client'
 
-const app: Application = express();
-app.use(bodyParser.json());
+const app: Application = express()
+app.use(bodyParser.json())
 
 // Configure o PrismaClient
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-const userRepository = new PrismaUserRepository(prisma);
-const userUseCase = new UserUseCase(userRepository);
-const userController = new UserController(userUseCase);
-const jwtSecret = process.env.JWT_SECRET || '';
+const userRepository = new PrismaUserRepository(prisma)
+const userUseCase = new UserUseCase(userRepository)
+const userController = new UserController(userUseCase)
+const jwtSecret = process.env.JWT_SECRET || ''
 
-const authService = new AuthService(userRepository, jwtSecret);
+const authService = new AuthService(userRepository, jwtSecret)
 
-app.post('/users', userController.createUser.bind(userController));
-app.get('/users/:id', userController.getUserById.bind(userController));
+app.post('/users', userController.createUser.bind(userController))
+app.get('/users/:id', userController.getUserById.bind(userController))
 
 app.post('/login', async (req: Request, res: Response) => {
-    const { email, senha } = req.body;
+  const { email, senha } = req.body
 
-    if (!email || !senha) {
-        return res.status(400).json({ error: 'Informe o email e a senha.' });
-    }
+  if (!email || !senha) {
+    return res.status(400).json({ error: 'Informe o email e a senha.' })
+  }
 
-    const token = await authService.login(email, senha);
+  const token = await authService.login(email, senha)
 
-    if (token) {
-        res.status(200).json({ token });
-    } else {
-        res.status(401).json({ error: 'Credenciais inválidas.' });
-    }
-});
+  if (token) {
+    res.status(200).json({ token })
+  } else {
+    res.status(401).json({ error: 'Credenciais inválidas.' })
+  }
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
