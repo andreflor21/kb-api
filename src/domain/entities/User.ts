0bf18113f49f54d29996db37528102afc1d046ce
Andre Flor <andreflor96@hotmail.com>
2023-11-09 23:37:01 -0300

    Add date type to date fields

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -3,14 +3,14 @@ class User {
     public nome: string,
     public email: string,
     public senha: string,
-    public dtCadastro: string,
+    public dtCadastro: string | Date,
     public perfilId: string,
     public id?: string,
     public cpf?: string | null,
     public ativo?: boolean,
-    public dtNascimento?: string | null,
+    public dtNascimento?: string | Date | null,
     public tokenReset?: string | null | undefined,
-    public tokenResetExpires?: string | null | undefined,
+    public tokenResetExpires?: string | Date | null | undefined,
     public trocaSenha?: boolean
   ) {}
 }
