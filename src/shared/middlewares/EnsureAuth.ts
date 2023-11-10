49422bb945ae8e86b0c8c6fbd66e1dbe993fe424
Andre Flor <andreflor96@hotmail.com>
2023-11-09 23:40:05 -0300

    change the return of ensureAuth function

--- src/shared/middlewares/EnsureAuth.ts
+++ src/shared/middlewares/EnsureAuth.ts
@@ -15,19 +15,20 @@ export default function ensureAuth(
   req: Request,
   res: Response,
   next: NextFunction
-): void {
+) {
   const authHeader = req.headers.authorization
   if (!authHeader) {
-    throw new AppError('Não Autorizado', 401)
+    return res.status(401).json({ error: 'Não Autorizado' })
   }
 
   try {
     const [, token] = authHeader.split(' ')
     const { secret } = authConfig.jwt
+
     const decoded = verify(token, secret)
     req.userId = (decoded as TokenPayload).userId // Adiciona o ID do usuário aos dados da solicitação
     next()
   } catch (error) {
-    throw new AppError('Token expirado ou não enviado', 401)
+    return res.status(401).json({ error: 'Token expirado ou não enviado' })
   }
 }
