56211db140428d83284b1e2b1ef8f1b5003bc0b1
Andre Flor <andreflor96@hotmail.com>
2023-11-09 23:36:19 -0300

    Error handdling

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express'
-import { User } from '@entities/User'
-import { IUserRepository } from '@repositories/IUserRepository'
-
+import { User } from '../../domain/entities/User'
+import { IUserRepository } from '../../domain/repositories/IUserRepository'
+import { serializeUser } from '../../infra/serializers/UserSerializer'
+import AppError from '../../shared/errors/AppError'
+import { AuthService } from '../services/AuthService'
 class UserController {
   constructor(private userRepository: IUserRepository) {}
 
@@ -10,11 +12,11 @@ class UserController {
       const user = req.body as User // Certifique-se de validar os dados de entrada
 
       const createdUser = await this.userRepository.createUser(user)
-      res.status(201).json(createdUser)
+      res.status(201).json(serializeUser(createdUser))
     } catch (error) {
-      console.log(error)
-
-      res.status(500).json({ error: 'Erro interno do servidor' })
+      if (error instanceof AppError)
+        res.status(error.statusCode).json({ error: error.message })
+      else res.status(500).json({ error: 'Erro interno do servidor' })
     }
   }
 
@@ -25,10 +27,41 @@ class UserController {
       if (!user) {
         res.status(404).json({ error: 'Usuário não encontrado' })
       } else {
-        res.status(200).json(user)
+        res.status(200).json(serializeUser(user))
       }
     } catch (error) {
+      if (error instanceof AppError) {
+        res.status(error.statusCode).json({ error: error.message })
+      } else res.status(500).json({ error: 'Erro interno do servidor' })
+    }
+  }
+
+  async getUsers(req: Request, res: Response) {
+    try {
+      const users = await this.userRepository.getUsers()
+
+      const serializedUsers = users.map((user) => serializeUser(user))
+      res.status(200).json(serializedUsers)
+    } catch (error) {
       res.status(500).json({ error: 'Erro interno do servidor' })
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    const jwtSecret = process.env.JWT_SECRET
+    const authService = new AuthService(this.userRepository, jwtSecret)
+    const { email, senha } = req.body
+
+    if (!email || !senha) {
+      return res.status(400).json({ error: 'Informe o email e a senha.' })
+    }
+
+    const token = await authService.login(email, senha)
+
+    if (token) {
+      res.status(200).json({ token })
+    } else {
+      res.status(401).json({ error: 'Credenciais inválidas.' })
     }
   }
   // Implemente outros métodos do controlador aqui
