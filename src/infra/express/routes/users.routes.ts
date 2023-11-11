1b701b1ab5b8600f693fe6033319404744e6c323
Andre Flor <andreflor96@hotmail.com>
2023-11-11 12:25:09 -0300

    change to portuguese endpoints add new routes [esqueci-senha, token, usuarios]

--- src/infra/express/routes/users.routes.ts
+++ src/infra/express/routes/users.routes.ts
@@ -13,10 +13,22 @@ const userRepository = new PrismaUserRepository(prisma)
 const userUseCase = new UserUseCase(userRepository)
 const userController = new UserController(userUseCase)
 
-userRoutes.post('/users', userController.createUser.bind(userController))
+userRoutes.post(
+  '/usuarios/novo',
+  userController.createUser.bind(userController)
+)
+userRoutes.post(
+  '/esqueci-senha',
+  userController.forgotPassword.bind(userController)
+)
 userRoutes.post('/login', userController.login.bind(userController))
+userRoutes.post(
+  '/usuarios/:token',
+  userController.recoverPassword.bind(userController)
+)
 
 userRoutes.use(ensureAuth)
-userRoutes.get('/users/:id', userController.getUserById.bind(userController))
+userRoutes.get('/usuarios/:id', userController.getUserById.bind(userController))
+userRoutes.get('/usuarios', userController.getUsers.bind(userController))
 
 export default userRoutes
