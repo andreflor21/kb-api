976a03c525027c299eecfeab0dc392aa62eebe20
Andre Flor <andreflor96@hotmail.com>
2023-11-11 12:27:26 -0300

    Add default error return

--- src/infra/express/server.ts
+++ src/infra/express/server.ts
@@ -1,12 +1,26 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import userRoutes from './routes/users.routes'
+import AppError from '../../shared/errors/AppError'
 
 const app: Application = express()
 app.use(bodyParser.json())
 
 app.use('/', userRoutes)
 
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  })
+})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
