74302623fda3e3fd9dd663953253d07bc1893269
Andre Flor <andreflor96@hotmail.com>
2024-08-24 09:38:15 -0300

    refactor: - audit the libs - changed the type of target from es2016 to es2020 - altered the interface for english in JWT token

--- src/@Types/fastify-jwt.ts
+++ src/@Types/fastify-jwt.ts
@@ -2,9 +2,9 @@ import '@fastify/jwt';
 
 declare module '@fastify/jwt' {
     export interface FastifyJWT {
-        usuario: {
+        user: {
             sub: string;
-            perfil: string;
+            profile: string;
         };
     }
 }
