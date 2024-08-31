be869856fa778d748b0e4fa80b4cea7dc11327cd
Andre Flor <andreflor96@hotmail.com>
2024-08-30 21:53:46 -0300

    fix: wrong type return add: error validation

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,64 +1,52 @@
 import { User, Prisma } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '../users-repository';
-import { AuthService } from '../../http/services/AuthService';
-import { hash } from 'bcryptjs';
+import { ExpiredTokenError } from '@/shared/errors/expired-token-error';
+import { UserNotFoundError } from '@/shared/errors/user-not-found-error';
+
 class PrismaUsersRepository implements UsersRepository {
-    async createUser(
-        data: Prisma.UserCreateInput
-    ): Promise<Omit<User, 'password'>> {
-        data.password = await hash(data.password, 10);
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
         const user = await prisma.user.create({ data });
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
+        return user;
     }
 
-    async getUserById(id: string): Promise<Omit<User, 'password'> | null> {
+    async getUserById(id: string): Promise<User | null> {
         const user = await prisma.user.findUnique({
             where: { id },
         });
         if (!user) return null;
 
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
+        return user;
     }
 
-    async getUserByToken(
-        token: string
-    ): Promise<Omit<User, 'password'> | null> {
+    async getUserByToken(token: string): Promise<User | null> {
         const user = await prisma.user.findFirst({
             where: { tokenReset: token },
         });
         if (!user) return null;
 
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
+        return user;
     }
 
-    async getUserByEmail(
-        email: string
-    ): Promise<Omit<User, 'password'> | null> {
+    async getUserByEmail(email: string): Promise<User | null> {
         const user = await prisma.user.findUnique({
             where: { email },
         });
         if (!user) return null;
 
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
+        return user;
     }
 
-    async getUsers(): Promise<Omit<User, 'password'>[]> {
+    async getUsers(): Promise<User[]> {
         const users = await prisma.user.findMany();
 
-        return users.map(
-            ({ password, ...userWithoutPassword }) => userWithoutPassword
-        );
+        return users;
     }
 
     async updateUser(
         id: string,
         data: Prisma.UserUpdateInput
-    ): Promise<Omit<User, 'password'> | null> {
+    ): Promise<User | null> {
         const user = await prisma.user.update({
             where: { id },
             data,
@@ -66,51 +54,64 @@ class PrismaUsersRepository implements UsersRepository {
 
         if (!user) return null;
 
-        const { password, ...userWithoutPassword } = user;
-        return userWithoutPassword;
+        return user;
     }
+
     async deleteUser(id: string): Promise<void> {
         const checkUser = await prisma.user.findUnique({
             where: { id },
         });
         if (checkUser) {
             await prisma.user.delete({ where: { id } });
+        } else {
+            throw new UserNotFoundError();
         }
     }
 
-    async changePassword(id: string, password: string): Promise<void> {
+    async changePassword(id: string, hashedPassword: string): Promise<void> {
         const checkUser = await prisma.user.findUnique({
             where: { id },
         });
 
-        const hashedPassword = await hash(password, 10);
-
         if (checkUser) {
             await prisma.user.update({
                 where: { id },
-                data: { password: hashedPassword },
+                data: { hashedPassword },
             });
+        } else {
+            throw new UserNotFoundError();
         }
     }
 
-    async recoverPassword(token: string, password: string): Promise<void> {
+    async recoverPassword(
+        token: string,
+        hashedPassword: string
+    ): Promise<void> {
         const checkUser = await prisma.user.findFirst({
             where: { tokenReset: token },
         });
-        const hashedPassword = await hash(password, 10);
 
         if (checkUser) {
+            const now = new Date();
+            const expires = new Date(
+                checkUser.tokenResetExpires ? checkUser.tokenResetExpires : ''
+            );
+            if (now > expires) throw new ExpiredTokenError();
+
             await prisma.user.update({
                 where: { id: checkUser.id },
                 data: {
-                    password: hashedPassword,
+                    hashedPassword,
                     tokenReset: null,
                     tokenResetExpires: null,
                     changePassword: false,
                 },
             });
+        } else {
+            throw new UserNotFoundError();
         }
     }
+
     async updateToken(
         userId: string,
         token: string,
@@ -129,9 +130,25 @@ class PrismaUsersRepository implements UsersRepository {
                     changePassword: true,
                 },
             });
+        } else {
+            throw new UserNotFoundError();
         }
     }
     // Implemente outros métodos de acesso ao banco aqui
+    async updateUserStatus(id: string, status: boolean): Promise<void> {
+        const checkUser = await prisma.user.findUnique({
+            where: { id },
+        });
+
+        if (checkUser) {
+            await prisma.user.update({
+                where: { id },
+                data: { active: status },
+            });
+        } else {
+            throw new UserNotFoundError();
+        }
+    }
 }
 
 export { PrismaUsersRepository };
