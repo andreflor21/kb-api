f4af7d1fae6742af510bf3bafc07a328fff667d1
Andre Flor <andreflor96@hotmail.com>
2024-08-30 21:52:47 -0300

    fix: removed "User" from factory name

--- src/use-cases/factories/make-change-password-use-case.ts
+++ src/use-cases/factories/make-change-password-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { ChangePasswordUseCase } from '../user/change-password';
 
-export function makeChangePasswordUserUseCase() {
+export function makeChangePasswordUseCase() {
     const userRepository = new PrismaUsersRepository();
     const changePasswordUseCase = new ChangePasswordUseCase(userRepository);
 
--- src/use-cases/factories/make-recover-password-use-case.ts
+++ src/use-cases/factories/make-recover-password-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { RecoverPasswordUseCase } from '../user/recover-password';
 
-export function makeRecoverPasswordUserUseCase() {
+export function makeRecoverPasswordUseCase() {
     const userRepository = new PrismaUsersRepository();
     const recoverPasswordUseCase = new RecoverPasswordUseCase(userRepository);
 
