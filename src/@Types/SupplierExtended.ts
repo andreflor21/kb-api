017e067a99031050bdd324345f859ec9f00f0cdb
Andre Flor <andreflor96@hotmail.com>
2024-09-22 17:47:30 -0300

    feat: Add new fields to SupplierExtended type
    This commit adds new fields to the SupplierExtended type, including addresses, users, deliveryDays, and products. These fields are now optional and can be used to store additional information related to a supplier.

--- src/@Types/SupplierExtended.ts
+++ src/@Types/SupplierExtended.ts
@@ -1,6 +1,14 @@
-import { Supplier, SupplierAddress, User } from '@prisma/client';
+import {
+    Supplier,
+    SupplierAddress,
+    User,
+    SupplierDeliveryDays,
+    Product,
+} from '@prisma/client';
 
 export type SupplierExtended = {
-    addresses: Partial<SupplierAddress>[];
-    users: Partial<User>[];
+    addresses?: Partial<SupplierAddress>[];
+    users?: Partial<User>[];
+    deliveryDays?: Partial<SupplierDeliveryDays>[];
+    products?: Partial<Product>[];
 } & Supplier;
