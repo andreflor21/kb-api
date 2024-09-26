9b38447a28d050ebeb8835f3063a32f8ab3f440d
Andre Flor <andreflor96@hotmail.com>
2024-09-26 17:12:07 -0300

    Refactor column types to use FLOAT instead of DECIMAL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
 	plugins: [tsconfigPaths()],
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "esnext", // Ensures modern JS is targeted
+			format: "esm", // Use ESM format
+		},
+	},
 	test: {
 		environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
 	},
